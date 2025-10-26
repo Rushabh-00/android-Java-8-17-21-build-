767f84148f40a0be54399c26a0b64ac6379e39e6
Rushabh <161584263+Rushabh-00@users.noreply.github.com>
2025-10-27 00:51:45 +0530

    Update ci_build_arch_aarch32.sh

--- ci_build_arch_aarch32.sh
+++ ci_build_arch_aarch32.sh
@@ -3,7 +3,6 @@ set -e
 
 export TARGET=arm-linux-androideabi
 export TARGET_JDK=arm
-export JVM_VARIANTS=server
 export NDK_PREBUILT_ARCH=/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/strip
 
 bash ci_build_global.sh
